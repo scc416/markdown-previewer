2f054bac7d0a1e2e8516895ef9b80b02ab220a58
Siu <siuc@protonmail.com>
2021-12-24 17:18:11 -0500

    start to remake the layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      Hello world
+    <div id="app">
+      <div
+        className="container"
+        onMouseMove={() => console.log("onMouseMove")} // onMouseMove = { this.props.dragMove }
+        onMouseUp={() => console.log("onMouseUp")} // onMouseUp = { this.props.dragEnd }
+      >
+        <div
+          id="left"
+          // style = {{ width: this.props.position.toString() + "%"}}
+        >
+          <div className="editor-title">
+            <i className="far fa-edit"></i> Editor
+          </div>
+          <textarea
+            id="editor"
+            // value={this.props.code}
+            // onChange={this.props.update}
+          ></textarea>
+        </div>
+        <div
+          id="border"
+          // style = {{left: this.props.position.toString() + "%"}}
+          // onMouseDown = {this.props.dragStart}
+        >
+          <div className="textbox">
+            <i className="fas fa-arrows-alt-h"></i>DRAG ME
+          </div>
+        </div>
+        <div
+          id="right"
+          // style={{ width: (100 - this.props.position).toString() + "%" }}
+        >
+          <div className="previewer-title">
+            <i className="fas fa-eye"></i> Preview
+          </div>
+          <div id="preview" />
+        </div>
+      </div>
     </div>
   );
 }
