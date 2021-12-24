dca089712699164e085df49fca1f04c5ae8bf6af
Siu <siuc@protonmail.com>
2021-12-24 18:41:07 -0500

    editing style

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,16 @@ const App = () => {
   const { code, position, updateInput, dragStart, dragMove, dragEnd, dragging } =
     useData();
   return (
-    <div id="app" style={{userSelect: !dragging}}>
+    <main style={{userSelect: !dragging}}>
       <div className="container" onMouseMove={dragMove()} onMouseUp={dragEnd}>
-        <div id="left" style={{ width: position + "%" }}>
+        <div className="left" style={{ width: position + "%" }}>
           <div className="editor-title">
             <i className="far fa-edit"></i> Editor
           </div>
-          <textarea id="editor" value={code} onChange={updateInput} disabled={dragging}></textarea>
+          <textarea className="editor" value={code} onChange={updateInput} disabled={dragging}></textarea>
         </div>
         <div
-          id="border"
+          className="border"
           style={{ left: position + "%" }}
           onMouseDown={dragStart}
           onTouchStart={dragStart}
@@ -30,12 +30,12 @@ const App = () => {
             <i className="fas fa-arrows-alt-h"></i>DRAG ME
           </div>
         </div>
-        <div id="right" style={{ width: 100 - position + "%" }}>
+        <div className="right" style={{ width: 100 - position + "%" }}>
           <div className="previewer-title">
             <i className="fas fa-eye"></i> Preview
           </div>
           <div
-            id="preview"
+            className="preview"
             dangerouslySetInnerHTML={createMarkup(
               marked(code, {
                 breaks: true,
@@ -44,7 +44,7 @@ const App = () => {
           />
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
